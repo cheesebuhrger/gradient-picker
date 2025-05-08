0a2f23f428b149dfc8f4c33e4081310d528cb50e
Buhr Duong <buhr.duong@gmail.com>
2025-05-08 16:27:29 -0500

    Update Vite configuration to include alias and optimize dependencies
    - Added alias for src directory to simplify imports.
    - Included 'gradient.js' in optimizeDeps for improved dependency handling.

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,11 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
+    alias: {
+      "@": "/src",
+    },
+  },
+  optimizeDeps: {
+    include: ["gradient.js"],
   },
 });
